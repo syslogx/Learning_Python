1d0e0cdeeedc3c467f479c96a56074e9ee457af9
Logan J <274869329+syslogx@users.noreply.github.com>
2026-04-09 12:50:07 -0400

    Add files via upload
    1st Python script: checks whether a given number is even or odd.

---
+++ first_script(number-check).py
@@ -0,0 +1,13 @@
+# First Python script, simply accepts any three-digit number and checks if it's even or odd. Will fail if number is more than three-digits.
+
+number = input("Enter a three digit number: ")
+number_length = len(number)
+
+if int(number) % 2 == 0 and number_length == 3:
+    print("Your number is even!")
+elif number_length == 3 and int(number) % 2 != 0:
+    print("Your number is odd.")
+else:
+    print("You didn't put a three digit number!")
+
+
