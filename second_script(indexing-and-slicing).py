7608c270ba6b8cdc3801d118cd2a044e4b667db3
Logan J <274869329+syslogx@users.noreply.github.com>
2026-04-09 14:41:41 -0400

    Update second_script(indexing-and-slicing).py
    Made more readable, added a single-character fix

--- second_script(indexing-and-slicing).py
+++ second_script(indexing-and-slicing).py
@@ -1,14 +1,15 @@
 # Second script, will accept a string input and detect if the word is a palindrome, matching first/last characters, or is just a normal word.
 # Key Takeaways: Using indexes and slices to manipulate and compare text, and using lower() to convert a string to lower-case
 
+user_input = input("Input a string: ")
 
-string1 = input("Input a string: ")
-
-if len(string1) == 0:
+if len(user_input) == 0:
     print("Empty string, try again.")
-elif string1.lower() == string1[::-1].lower():
+elif len(user_input) == 1:
+    print("String is single-character, try a longer string.")
+elif user_input.lower() == user_input[::-1].lower():
     print("Mirror word")
-elif string1[0] == string1[-1]:
+elif user_input[0].lower() == user_input[-1].lower():
     print("Starts and ends same")
 else:
     print("Not special")
