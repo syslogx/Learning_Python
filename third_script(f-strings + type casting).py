497d09ca11422d3531696ec8fbe9e4c5d67a4224
Logan J <274869329+syslogx@users.noreply.github.com>
2026-04-10 12:42:57 -0400

    3rd Script - Convenience Store
    Created this script to work with f-strings and type casting to simulate a store environment.

--- third_script(f-strings + type casting).py
+++ third_script(f-strings + type casting).py
@@ -1,6 +1,11 @@
+# Third script: Will simulate a store interaction, by asking the user for their item, 
+#               the price of that item, and how many they want to purchase
+# Key Takeaways: Using f-strings to call variables or perform calculations in the same string, 
+#                and using type casting to change inputs to float/int for calculations.
+
 print("Welcome to the shop!")
 item = input("What item are you looking for? ")
-price = input(f"What is the price of {item}?")
+price = input(f"What is the price of {item}? ")
 quantity = input(f"How many of {item} are you purchasing? ")
 
 print(f"Added {quantity} {item}(s) to shopping cart \n Subtotal: ${float(price) * float(quantity)}")
