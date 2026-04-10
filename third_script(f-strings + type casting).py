000aa55d89cae87e32a7b718820e2bd775bb83d8
Logan J <274869329+syslogx@users.noreply.github.com>
2026-04-10 13:07:55 -0400

    3rd Script - Store Simulation

--- third_script(f-strings + type casting).py
+++ third_script(f-strings + type casting).py
@@ -5,7 +5,7 @@
 
 print("Welcome to the shop!")
 item = input("What item are you looking for? ")
-price = input(f"What is the price of {item}? ")
-quantity = input(f"How many of {item} are you purchasing? ")
+price = float(input(f"What is the price of {item}? "))
+quantity = float(input(f"How many of {item} are you purchasing? "))
 
-print(f"Added {quantity} {item}(s) to shopping cart \n Subtotal: ${float(price) * float(quantity)}")
+print(f"Added {quantity} {item}(s) to shopping cart \n Subtotal: ${price * quantity}")
